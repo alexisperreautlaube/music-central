def77792ad8cb274607768baf716d6433a70dc45
alexisperreaultaube <alexis.perreault.aube@gmail.com>
2021-11-14 16:02:19 -0500

    release commit

--- script/release.sh
+++ script/release.sh
@@ -63,7 +63,7 @@ function usage() {
 # HANDLE COMMAND-LINE OPTIONS #
 ###############################
 
-while getopts "ahr:n:c:i:d:" o; do
+while getopts "ahdr:n:c:i:" o; do
 	case "${o}" in
 		a)
 			RELEASE_VERSION="auto"
