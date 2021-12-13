1ccfbcbcb7e43f0bfc7ddcf4598120265554f91c
alexisperreaultaube <alexis.perreault.aube@gmail.com>
2021-12-13 17:34:06 -0500

    release commit auto

--- script/backup-mongo.sh
+++ script/backup-mongo.sh
@@ -1,6 +1,7 @@
-/bin/sh
+#!/bin/bash
 rm -rf /tmp/local
 mkdir /tmp/local
-mongodump --db local --host 192.168.1.163 --out "/tmp/local"
+mongodump --db local --host 192.168.1.163 --out /tmp/local --forceTableScan
 today=$(date +"%Y-%m-%d")
 zip -r /mnt/backup/mongodb/kube-mongo-$today /tmp/local/
+rm -rf /tmp/local
