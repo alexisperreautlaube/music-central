96247ac9eabcf3b2ccb829ea058d7028c7ecaced
alexisperreaultaube <alexis.perreault.aube@gmail.com>
2022-12-18 15:38:06 -0500

    release 2.8.5

--- mc-messaging/mc-msg-consumer/src/main/java/com/apa/consumer/config/MusicCentralImporterConfiguration.java
+++ mc-messaging/mc-msg-consumer/src/main/java/com/apa/consumer/config/MusicCentralImporterConfiguration.java
@@ -9,13 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
-@EnableWebMvc
 @Configuration
 @Import({
         MusicCentralEventsConfiguration.class,
@@ -30,13 +24,5 @@ public class MusicCentralImporterConfiguration {
         return mapper;
     }
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
 }
 
--- mc-messaging/mc-msg-consumer/src/test/java/com/apa/consumer/AbstractConsumerIT.java
+++ mc-messaging/mc-msg-consumer/src/test/java/com/apa/consumer/AbstractConsumerIT.java
@@ -4,11 +4,9 @@ import com.apa.importer.ImporterSpringBootTestApplication;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 
-@EnableWebMvc
 @Testcontainers
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = ImporterSpringBootTestApplication.class)
